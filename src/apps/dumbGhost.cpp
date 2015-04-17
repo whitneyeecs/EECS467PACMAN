4d09e4186985d71c92bbd4f5c50c44d0c11ac7dc
whitneyeecs <smithwe@umich.edu>
2015-04-17 00:13:10 -0400

    ghost AIs done

--- src/apps/dumbGhost.cpp
+++ src/apps/dumbGhost.cpp
@@ -6,6 +6,7 @@
 #include <pthread.h>
 #include <string.h>
 #include <math.h>
+#include <stack>
 
 #include "common/zarray.h"
 
@@ -16,57 +17,57 @@
 
 //lcm
 #include "lcm/lcm-cpp.hpp"
-#include "lcmtypes/maebot_board_locations_t.hpp"
+#include <lcmtypes/maebot_pose_t.hpp>
 
 #include "eecs467_util.h"    // This is where a lot of the internals live
 
+#include "math/point.hpp"
+
 #include "a3/navigation.hpp"
-#include "a3/map.hpp"
+#include "a3/board.hpp"
+#include "a3/Constants.hpp"
+
+using namespace eecs467;
 
 typedef struct state state_t;
 struct state {
  
    	bool running;
 
-
 	image_u32_t image;
 
+	//game stuff
 	Navigation* nav;
+	Board board;
+ 
+	int diffy; // larger the number the easier, the smaller the harder
+	int diffyCount;
 
+	stack < Point<int> > wayPoints; 
 
-    // threads
+    //threads
     pthread_t odo_thread;
 	pthread_t command_thread;
-
-	//map
-	Map map;
+	pthread_t ai_thread;
 
     // for accessing the arrays
     pthread_mutex_t mutex;
 
-
 	lcm::LCM* lcm;
 
-	void handle_command(const lcm::ReceiveBuffer* rbuf,
-				const std::string& chan, 
-				const pacman_command_t* msg){
-		if(msg->command == 1.0){
-			nav->stop();
-		}
-		else if(msg->command == END_PROG){
-			std::cout <<"Should End" << std::endl;
-			running = false;
-		}
-		else{
-			std::cout << "GOT A MESSAGE" << std::endl;
-			nav->go(msg->command);
-		}
+	maebot_pose_t pose;
+
+	void pose_handler(const lcm::ReceiveBuffer* rbuf, const std::string& chan,
+					   const maebot_pose_t* msg)
+	{
+		pose = *msg;
+		nav->push_pose(*msg);
 	}
+
 };
 
-
 void* command_thread(void* arg){
-	int hz = 20;
+	int hz = 50;
 	state_t* state = (state_t*) arg;
 	while(state->running){
 		state->nav->publish();
@@ -75,8 +76,7 @@ void* command_thread(void* arg){
 	return NULL;
 }
 
-void *
-odo_thread (void *data)
+void * odo_thread (void *data)
 {
     state_t *state = (state_t*)data;
 
@@ -87,13 +87,48 @@ odo_thread (void *data)
     return NULL;
 }
 
+void * ai_thread(void *data)
+{
+	state_t *state = (state_t*)data;
+	
+	//RANDOM EXPLORE
+	while(!state->nav->is_driving()){
+	
+		Point <int> myLocation = state->board.convertToBoardCoords(state->pose);	
+		
+		if(state->diffyCount > state->diffy || state->wayPoints.empty()) {
+			//get new path
+			Point <int> dest;
+			dest.x = rand() % state->board.width;
+			dest.y = rand() % state->board.height;
+			state->wayPoints = state->board.getPath(myLocation, dest);			
+			state->diffyCount = 0;
+		}
+		
+		else {
+			Point <float> point;
+			point.x = state->wayPoints.top().x;
+			point.y = state->wayPoints.top().y;
+			state->wayPoints.pop();
+			Point <float> dest = state->board.convertToGlobalCoords(point);
+			state->nav->go_to(dest);			
+			state->diffyCount++;
+		}	
+	}
+	
+	return NULL;
+}
+
 state_t *
 state_create (void)
 {
    	state_t *state = (state_t*)calloc (1, sizeof(*state));
 
    	state->running = 1;
-	state->nav = new Navigation;
+	state->nav = new Navigation(DUMBGHOST);
+	
+	state->diffyCount = 0;
+	state->diffy = 2;
    	
 	state->lcm = new lcm::LCM; 
     	if(!state->lcm->good()){
@@ -101,9 +136,9 @@ state_create (void)
 		exit(1);
 	}
 
-	state->lcm->subscribe("PACMAN_COMMAND", &state::handle_command, state);
-    
-    	return state;
+	state->lcm->subscribe("DUMBGHOST_POSE", &state::pose_handler, state);    
+
+    return state;
 }
 
 void
@@ -113,8 +148,6 @@ state_destroy (state_t *state)
         return;
 
 	delete state->nav;
-	delete state->lcm;
-
 
     free (state);
 }
@@ -125,12 +158,10 @@ main (int argc, char *argv[])
 //	eecs467_init (argc, argv);
 	state_t *state = state_create ();
 
-    
-
-
 	// Launch our worker threads
 	pthread_create (&state->odo_thread, NULL, odo_thread, state);
 	pthread_create (&state->command_thread, NULL, command_thread, state);
+	pthread_create (&state->ai_thread, NULL, ai_thread, state);
 
 	while(state->running){
 		state->lcm->handle();
@@ -138,23 +169,10 @@ main (int argc, char *argv[])
 
 	pthread_join (state->odo_thread, NULL);
 	pthread_join (state->command_thread, NULL);
+	pthread_join (state->ai_thread, NULL);
 
 	// Cleanup
 	state_destroy (state);
 
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/apps/smartGhost.cpp
+++ src/apps/smartGhost.cpp
@@ -6,6 +6,7 @@
 #include <pthread.h>
 #include <string.h>
 #include <math.h>
+#include <stack>
 
 #include "common/zarray.h"
 
@@ -16,57 +17,60 @@
 
 //lcm
 #include "lcm/lcm-cpp.hpp"
-#include "lcmtypes/maebot_board_locations_t.hpp"
+#include <lcmtypes/maebot_pose_t.hpp>
 
 #include "eecs467_util.h"    // This is where a lot of the internals live
 
+#include "math/point.hpp"
+
 #include "a3/navigation.hpp"
-#include "a3/map.hpp"
+#include "a3/board.hpp"
+#include "a3/Constants.hpp"
+
+using namespace eecs467;
 
 typedef struct state state_t;
 struct state {
  
    	bool running;
 
-
 	image_u32_t image;
 
+	//game stuff
 	Navigation* nav;
+	Board board;
 
+	stack < Point<int> > wayPoints; 
 
-    // threads
-   	pthread_t odo_thread;
+    //threads
+    pthread_t odo_thread;
 	pthread_t command_thread;
+	pthread_t ai_thread;
 
-	//map
-	Map map;
-
-    // for accessing the arrays
     pthread_mutex_t mutex;
-
 
 	lcm::LCM* lcm;
 
-	void handle_command(const lcm::ReceiveBuffer* rbuf,
-				const std::string& chan, 
-				const pacman_command_t* msg){
-		if(msg->command == 1.0){
-			nav->stop();
-		}
-		else if(msg->command == END_PROG){
-			std::cout <<"Should End" << std::endl;
-			running = false;
-		}
-		else{
-			std::cout << "GOT A MESSAGE" << std::endl;
-			nav->go(msg->command);
-		}
+	maebot_pose_t pose;
+	Point <int> pacmanDest;
+
+	void pose_handler(const lcm::ReceiveBuffer* rbuf, const std::string& chan,
+					   const maebot_pose_t* msg)
+	{
+		pose = *msg;
+		nav->push_pose(*msg);
 	}
+
+	void pacmanDest_handler(const lcm::ReceiveBuffer* rbuf, 
+								const std::string& chan, const Point <int> *msg)
+	{
+		pacmanDest = *msg;
+	}
+	
 };
 
-
 void* command_thread(void* arg){
-	int hz = 20;
+	int hz = 50;
 	state_t* state = (state_t*) arg;
 	while(state->running){
 		state->nav->publish();
@@ -75,8 +79,7 @@ void* command_thread(void* arg){
 	return NULL;
 }
 
-void *
-odo_thread (void *data)
+void * odo_thread (void *data)
 {
     state_t *state = (state_t*)data;
 
@@ -87,23 +90,48 @@ odo_thread (void *data)
     return NULL;
 }
 
+void * ai_thread(void *data)
+{
+	state_t *state = (state_t*)data;
+
+	//THE CHASE!!!
+	while(!state->nav->is_driving()){
+
+		Point <int> myLocation = state->board.convertToBoardCoords(state->pose);	
+		
+		state->wayPoints = state->board.getPath(myLocation, state->pacmanDest);
+		
+		if(!state->wayPoints.empty()) {	
+			Point <int> point;
+			point.x = state->wayPoints.top().x;
+			point.y = state->wayPoints.top().y;
+			state->wayPoints.pop();
+			Point <float> dest = state->board.convertToGlobalCoords(point); 
+			state->nav->go_to(dest);
+		}	
+	}
+
+	return NULL;
+}
+
 state_t *
 state_create (void)
 {
    	state_t *state = (state_t*)calloc (1, sizeof(*state));
 
    	state->running = 1;
-	state->nav = new Navigation;
+	state->nav = new Navigation(SMARTGHOST);
    	
 	state->lcm = new lcm::LCM; 
     	if(!state->lcm->good()){
 		std::cout <<"error initializing LCM !!!" << std::endl;
 		exit(1);
 	}
+    	
+	state->lcm->subscribe("SMARTGHOST_POSE", &state::pose_handler, state);
+    //state->lcm->subscribe("PACMAN_DEST", &state::pacmanDest_handler, state);
 
-	state->lcm->subscribe("PACMAN_COMMAND", &state::handle_command, state);
-    
-    	return state;
+    return state;
 }
 
 void
@@ -113,8 +141,6 @@ state_destroy (state_t *state)
         return;
 
 	delete state->nav;
-	delete state->lcm;
-
 
     free (state);
 }
@@ -125,12 +151,10 @@ main (int argc, char *argv[])
 //	eecs467_init (argc, argv);
 	state_t *state = state_create ();
 
-    
-
-
 	// Launch our worker threads
 	pthread_create (&state->odo_thread, NULL, odo_thread, state);
 	pthread_create (&state->command_thread, NULL, command_thread, state);
+	pthread_create (&state->ai_thread, NULL, ai_thread, state);
 
 	while(state->running){
 		state->lcm->handle();
@@ -138,23 +162,10 @@ main (int argc, char *argv[])
 
 	pthread_join (state->odo_thread, NULL);
 	pthread_join (state->command_thread, NULL);
+	pthread_join (state->ai_thread, NULL);
 
 	// Cleanup
 	state_destroy (state);
 
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
