f7992725bc03cadad5f03609dddf3088e4b14e1a
Charles Hardin <chhardin@umich.edu>
2015-04-20 02:55:06 -0400

    AHHHH

--- src/a3/RobotConstants.hpp
+++ src/a3/RobotConstants.hpp
@@ -23,7 +23,7 @@ static const int maxNumLasersPerScan = 400; // a max number of laser readings pe
  * @return theta in robot frame in radians
  */
 static inline float laserThetaToMaebotTheta(float laserTheta) {
-	return -laserTheta;
+	return -laserTheta + (M_PI / 105.0);
 }
 
 }
